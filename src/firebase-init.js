f0e7c7ee9893beab1e7cea29beac803de267ae68
hhdyer96792-cmd <hhdyer96792@gmail.com>
2026-06-13 00:45:47 +0400

    Create firebase-init.js

---
+++ src/firebase-init.js
@@ -0,0 +1,13 @@
+// src/firebase-init.js
+// Инициализация Firebase для клиентской части (нужна для получения токена)
+if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCKz1GKDdqxtK6NyLQAZ84QqUUCaqTQDWQ",
+    authDomain: "car-k3eper.firebaseapp.com",
+    projectId: "car-k3eper",
+    storageBucket: "car-k3eper.firebasestorage.app",
+    messagingSenderId: "826833638199",
+    appId: "1:826833638199:web:647fedbe3eae5b605240b2"
+  });
+  console.log('[Firebase] Инициализирован в основном потоке');
+}
